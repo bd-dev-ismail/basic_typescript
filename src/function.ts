10c9a9267f8cfcfc1e103e659b7bc8857f8e6e0a
bd-dev-ismail <ismailratul7@gmail.com>
2023-05-01 22:03:51 +0600

    array & object destructoring

--- src/function.ts
+++ src/function.ts
@@ -1,8 +1,9 @@
 //normal function
-function add(num1:number, num2:number):number{
+//defult parameteer
+function add(num1:number, num2:number = 60):number{
     return num1 + num2;
 }
-add(2, 2)
+add( 2)
 const sum = (num11:number, num22:number):number=> {
     return num11 + num22
 }
@@ -11,10 +12,33 @@ const arr = [1, 3, 4, 5];
 
 const newArray = arr.map((element : number) => element*element);
 
-const person:{name:string, blance:number,addBlance(money:number):void} = {
-    name: "Ratul",
-    blance: 5,
-    addBlance(money: number){
-        console.log(`My Total blance is ${this.blance+money}`)
-    }
+// const person:{name:string, blance:number,addBlance(money:number):void} = {
+//     name: "Ratul",
+//     blance: 5,
+//     addBlance(money: number){
+//         console.log(`My Total blance is ${this.blance+money}`)
+//     }
+// }
+
+//speard operator 
+// const friends = ['asif', 'sakib', 'jhon', 'doe'];
+// const newFriends = ['sbala', 'babla', 'fariha', 'sujitha'];
+// friends.push(...newFriends)
+// console.log(friends)
+// rest operator
+// const greetFriends = (...friends: string[]):void=> {
+//     friends.forEach(fd => console.log(`Hi ${fd}`))
+// };
+// greetFriends("Nowrin", "Noushin", "Amrin", "Rodela", "ayat", "Babe", "kolijja");
+
+//array & object destructor
+const friends = ['asif', 'sakib', 'jhon', 'doe'];
+const newFriends = ['sbala', 'babla', 'fariha', 'sujitha'];
+const myBestFriends = {
+    fullName: "Md Ismail Hossen",
+    age: 22,
+
 }
+const [bestFriends] = friends;
+const {fullName} = myBestFriends
+

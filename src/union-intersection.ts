bc474a4ccedcf3effbcefbd60a22b0980cfabe95
bd-dev-ismail <ismailratul7@gmail.com>
2023-05-02 14:33:30 +0600

    union & intersection

---
+++ src/union-intersection.ts
@@ -0,0 +1,38 @@
+type NoobDeveloper = {
+  name: string;
+};
+// type JuniorDeveloper = {
+//     name: string,
+//     experties: string,
+//     experience: number,
+// }
+/// & dawa k bola hocce intersection
+type JuniorDeveloper = NoobDeveloper & {
+  experties: string;
+  experience: number;
+};
+/// orr kora ke bolteci union
+
+enum Level {
+  Junior = "Junior",
+  Mid = "Mid",
+  Senior = "Senior",
+}
+
+type NextLevelDeveloper = JuniorDeveloper & {
+  leaderShip: number;
+  level: Level;
+};
+
+const newDeveloper: NoobDeveloper | JuniorDeveloper = {
+  name: "Ismail Hossen",
+  experties: "JavaScript",
+  experience: 6,
+};
+
+const developer: JuniorDeveloper | NextLevelDeveloper = {
+  name: "Ismail Hossen",
+  experties: "TypeScript",
+  experience: 4,
+  level: Level.Mid,
+};

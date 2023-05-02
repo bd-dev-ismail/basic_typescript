8e65d21fb58f64081d825d023215f27802572bf0
bd-dev-ismail <ismailratul7@gmail.com>
2023-05-02 22:29:05 +0600

    exploring type assertion

---
+++ src/advance_type_of_typescript/type-assertion.ts
@@ -0,0 +1,26 @@
+let emni: any;
+emni = "Next Level Web Development";
+
+// (emni as string).charAt;
+<string>emni.charAt;
+function kgToGm(param: string | number): string | number | undefined {
+  if (typeof param === "string") {
+    const value = parseFloat(param) * 1000;
+    return `The Converted value is ${value} Gram`;
+  }
+  if (typeof param === "number") {
+    const value = param * 1000;
+    return `The Converted Value is ${value} Gram`;
+  }
+}
+const resultTobeNumber = <number>kgToGm(20);
+const resultTobeString = <string>kgToGm("20");
+
+type CustomErrorType = {
+  message: string;
+};
+
+try {
+} catch (err) {
+  console.log((err as CustomErrorType).message);
+}

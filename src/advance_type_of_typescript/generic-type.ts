a7c2ef35144a0a42d0438bbc6368f481c992a41e
bd-dev-ismail <ismailratul7@gmail.com>
2023-05-03 12:50:04 +0600

    generic type & generic interface exploreing

--- src/advance_type_of_typescript/generic-type.ts
+++ src/advance_type_of_typescript/generic-type.ts
@@ -1,27 +1,32 @@
-//generic Tuplle
-type GenericTuple<X, Y> = [X, Y];
+// //generic Tuplle
+// type GenericTuple<X, Y> = [X, Y];
 
-const relation: GenericTuple<string, string> = ["Ismail", "Noushin"];
+// const relation: GenericTuple<string, string> = ["Ismail", "Noushin"];
+// //using type alis
+// // type RelationOjbSalary = { name: string; salary: number };
+// //using interface
+// interface IReelationOjbSalary {
+//   name: string;
+//   salary: number;
+// }
 
-type RelationOjbSalary = { name: string; salary: number }
-const relationWithSalary: GenericTuple<RelationOjbSalary, string> = [
-  { name: "Ismail", salary: 10 },
-  "Noushin",
-];
+// const relationWithSalary: GenericTuple<IReelationOjbSalary, string> = [
+//   { name: "Ismail", salary: 10 },
+//   "Noushin",
+// ];
 
+// const relationWithSalary2: GenericTuple<IReelationOjbSalary, string> = [
+//   { name: "Ismail", salary: 10 },
+//   "Noushin",
+// ];
 
-const relationWithSalary2: GenericTuple<RelationOjbSalary, string> = [
-  { name: "Ismail", salary: 10 },
-  "Noushin",
-];
-
-type GenericArray<T> = Array<T>;
-const rollNumberss: Array<number> = [23, 234, 24, 234, 234, 23];
-const rollNumber2: GenericArray<string> = ["44", "55", "66"];
-const rollNumber3: GenericArray<boolean> = [true, true, false];
-//
-type NameRollType = { name: string; roll: number };
-const userRollAndNumbers: GenericArray<NameRollType> = [
-  { name: "Mr. X", roll: 23 },
-  { name: "MR.Y", roll: 345 },
-];
+// type GenericArray<T> = Array<T>;
+// const rollNumberss: Array<number> = [23, 234, 24, 234, 234, 23];
+// const rollNumber2: GenericArray<string> = ["44", "55", "66"];
+// const rollNumber3: GenericArray<boolean> = [true, true, false];
+// //
+// type NameRollType = { name: string; roll: number };
+// const userRollAndNumbers: GenericArray<NameRollType> = [
+//   { name: "Mr. X", roll: 23 },
+//   { name: "MR.Y", roll: 345 },
+// ];

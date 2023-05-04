59cb782798a37dcca91426a77537732658159d32
bd-dev-ismail <ismailratul7@gmail.com>
2023-05-04 15:43:48 +0600

    conditional typescript

--- src/advance_type_of_typescript/asychoronus.ts
+++ src/advance_type_of_typescript/asychoronus.ts
@@ -1,73 +1,73 @@
-//mocking - string
-///data fetch with joson placeholder
-interface ITodo {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-}
-const getTodo = async (): Promise<ITodo> => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-  return await res.json();
-};
+// //mocking - string
+// ///data fetch with joson placeholder
+// interface ITodo {
+//   userId: number;
+//   id: number;
+//   title: string;
+//   completed: boolean;
+// }
+// const getTodo = async (): Promise<ITodo> => {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+//   return await res.json();
+// };
 
-const getTodoData = async (): Promise<void> => {
-  const result = await getTodo();
-  console.log(result);
-};
-getTodoData();
+// const getTodoData = async (): Promise<void> => {
+//   const result = await getTodo();
+//   console.log(result);
+// };
+// getTodoData();
 
-const makePromise = (): Promise<string> => {
-  return new Promise<string>((resolve, reject) => {
-    const data: string = "Data is fetch";
-    if (data) {
-      resolve(data);
-    } else {
-      reject("Promise failed!! failed to fetch");
-    }
-  });
-};
-/// promise type boolean
-const makePromiseBoolean = (): Promise<boolean> => {
-  return new Promise<boolean>((resolve, reject) => {
-    const data: boolean = true;
-    if (data) {
-      resolve(data);
-    } else {
-      reject("Promise failed!! failed to fetch");
-    }
-  });
-};
-type DataType = { status: boolean; data: string };
-interface IData {
-  status: boolean;
-  data: string;
-}
-/// promise type object
-const makePromiseObject = (): Promise<IData> => {
-  return new Promise<IData>((resolve, reject) => {
-    const data: IData = { status: true, data: "data fetch" };
-    if (data) {
-      resolve(data);
-    } else {
-      reject("Promise failed!! failed to fetch");
-    }
-  });
-};
+// const makePromise = (): Promise<string> => {
+//   return new Promise<string>((resolve, reject) => {
+//     const data: string = "Data is fetch";
+//     if (data) {
+//       resolve(data);
+//     } else {
+//       reject("Promise failed!! failed to fetch");
+//     }
+//   });
+// };
+// /// promise type boolean
+// const makePromiseBoolean = (): Promise<boolean> => {
+//   return new Promise<boolean>((resolve, reject) => {
+//     const data: boolean = true;
+//     if (data) {
+//       resolve(data);
+//     } else {
+//       reject("Promise failed!! failed to fetch");
+//     }
+//   });
+// };
+// type DataType = { status: boolean; data: string };
+// interface IData {
+//   status: boolean;
+//   data: string;
+// }
+// /// promise type object
+// const makePromiseObject = (): Promise<IData> => {
+//   return new Promise<IData>((resolve, reject) => {
+//     const data: IData = { status: true, data: "data fetch" };
+//     if (data) {
+//       resolve(data);
+//     } else {
+//       reject("Promise failed!! failed to fetch");
+//     }
+//   });
+// };
 
-// const result1 = makePromise();
+// // const result1 = makePromise();
 
-const getPromiseData = async (): Promise<string> => {
-  const data = await makePromise();
-  return data;
-};
-const getPromiseDataBoolean = async (): Promise<boolean> => {
-  const data = await makePromiseBoolean();
-  return data;
-};
-const getPromiseDataObject = async (): Promise<IData> => {
-  const data = await makePromiseObject();
-  return data;
-};
+// const getPromiseData = async (): Promise<string> => {
+//   const data = await makePromise();
+//   return data;
+// };
+// const getPromiseDataBoolean = async (): Promise<boolean> => {
+//   const data = await makePromiseBoolean();
+//   return data;
+// };
+// const getPromiseDataObject = async (): Promise<IData> => {
+//   const data = await makePromiseObject();
+//   return data;
+// };
 
-//Promise<string> Promise<boolean> Promise<>
+// //Promise<string> Promise<boolean> Promise<>
--- src/advance_type_of_typescript/generic-key-of-constrains.ts
+++ src/advance_type_of_typescript/generic-key-of-constrains.ts
@@ -1,21 +1,21 @@
-type PersonType = {
-  name: string;
-  age: number;
-  address: string;
-};
+// type PersonType = {
+//   name: string;
+//   age: number;
+//   address: string;
+// };
 
-type newType = "name" | "age" | "address";
+// type newType = "name" | "age" | "address";
 
-type newTypeUsingKeyOf = keyof PersonType;
+// type newTypeUsingKeyOf = keyof PersonType;
 
-// const a: newType = "address"
-// const b: newTypeUsingKeyOf = "name"
+// // const a: newType = "address"
+// // const b: newTypeUsingKeyOf = "name"
 
-function getProperty<X, Y extends keyof X>(obj: X, key: Y) {
-  obj[key];
-}
+// function getProperty<X, Y extends keyof X>(obj: X, key: Y) {
+//   obj[key];
+// }
 
-const property = getProperty({ name: "Hello", age: 33 }, "name");
-// ({name: "Mr. Hello", age: 234}, "age") //100
-const a = { name: "Mr. Hello", age: 234 };
-// a["age"]
+// const property = getProperty({ name: "Hello", age: 33 }, "name");
+// // ({name: "Mr. Hello", age: 234}, "age") //100
+// const a = { name: "Mr. Hello", age: 234 };
+// // a["age"]

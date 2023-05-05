2234f9b52180ad166794c5e2fe69120c1a33f6aa
bd-dev-ismail <ismailratul7@gmail.com>
2023-05-05 15:12:00 +0600

    getter & setter

---
+++ src/Module-4-OOP/getter-setter.ts
@@ -0,0 +1,34 @@
+class BankAccount {
+  public readonly id: number;
+  public name: string;
+  private _blance: number;
+  constructor(id: number, name: string, blance: number) {
+    (this.id = id), (this.name = name), (this._blance = blance);
+  }
+  ///getter bosabo
+  get blance(): number{
+    return this._blance
+  }
+//   getBlance(): number {
+//     return this._blance
+//   }
+///setter
+set deposite(amount: number){
+    this._blance = this._blance + amount;
+}
+  addDeposite(amount: number) {
+    this._blance = this._blance + amount;
+  }
+}
+
+class StudentAccount extends BankAccount {
+  test() {
+    
+  }
+}
+
+const myAccount = new BankAccount(213, "Ismail hossen", 23);
+// myAccount.blance = 0;
+console.log(myAccount.blance);
+myAccount.deposite = 39;
+console.log(myAccount.blance)

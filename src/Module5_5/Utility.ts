cc1ec3561350e1e8192bcfefb5d101d9382a2060
bd-dev-ismail <ismailratul7@gmail.com>
2023-05-11 21:24:14 +0600

    exploring utilty ty[pe

--- src/Module5_5/Utility.ts
+++ src/Module5_5/Utility.ts
@@ -18,3 +18,29 @@ type optional = Partial<Personnn>
 
 //required 
 type requiredPeroperties = Required<Personnn>
+
+const person  : Readonly<Personnn> ={
+    name: "Ismail",
+    email: "ismail@gmail.com",
+    contactNumber: 38492390290,
+}
+
+// type myObj = {
+//     a: string,
+//     b: string,
+//     c: string,
+// }
+//using index signiture
+// type myObj = {
+//   [key: "a" | "b" | "c"]: string;
+// };
+//utility type er recoder type
+//Record type "a" | "b" | "c"]
+type myObj = Record<"a" | "b" | "c" | "d", string>
+
+const obj : myObj ={
+    a: "1",
+    b: "2",
+    c: "3",
+    d: "3",
+}
